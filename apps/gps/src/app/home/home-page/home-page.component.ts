7df31ba85d949bd0a31a6d13142787b8696ed62a
Olivia <olivia.guyot@camptocamp.com>
2025-04-03 13:36:21 +0200

    use gn-ui map instead of geoadmin iframe

--- apps/gps/src/app/home/home-page/home-page.component.ts
+++ apps/gps/src/app/home/home-page/home-page.component.ts
@@ -1,11 +1,14 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, importProvidersFrom, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeSearchComponent } from '../home-search/home-search.component'
 import {
-  FeatureSearchModule,
+  BASEMAP_LAYERS,
+  FeatureSearchModule, MapFacade, MapStateContainerComponent,
   RESULTS_LAYOUT_CONFIG,
   ResultsLayoutConfigItem,
   SearchFacade,
+  DO_NOT_USE_DEFAULT_BASEMAP,
+  MAP_VIEW_CONSTRAINTS, FeatureMapModule,
 } from 'geonetwork-ui'
 import { CatalogRecord } from 'geonetwork-ui/libs/common/domain/src/lib/model/record'
 import { RecordPreviewRowComponent } from '../record-preview-row/record-preview-row.component'
@@ -13,7 +16,13 @@ import { RecordPreviewRowComponent } from '../record-preview-row/record-preview-
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, HomeSearchComponent, FeatureSearchModule],
+  imports: [
+    CommonModule,
+    HomeSearchComponent,
+    FeatureSearchModule,
+    MapStateContainerComponent,
+    FeatureMapModule,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,14 +38,39 @@ import { RecordPreviewRowComponent } from '../record-preview-row/record-preview-
         ),
       },
     },
+    {
+      provide: BASEMAP_LAYERS,
+      useValue: [{
+        type: "xyz",
+        url: "https://wmts10.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-grau/default/current/3857/{z}/{x}/{y}.jpeg"
+      }]
+    },
+    {
+      provide: DO_NOT_USE_DEFAULT_BASEMAP,
+      useValue: true
+    },
+    {
+      provide: MAP_VIEW_CONSTRAINTS,
+      useValue: {
+        maxExtent: [320000, 5500000, 1580000, 6300000]
+      }
+    }
   ],
 })
 export class HomePageComponent implements OnInit {
-  constructor(private searchFacade: SearchFacade) {}
+  constructor(private searchFacade: SearchFacade, private mapFacade: MapFacade) {}
 
   ngOnInit() {
     this.searchFacade.setResultsLayout('ROW').setPageSize(6)
+    this.mapFacade.applyContext({
+      layers: [],
+      view: {
+        zoom: 2,
+        center: [8.4265137, 46.8075795]
+      },
+    })
   }
 
-  onMetadataSelection($event: CatalogRecord) {}
+  onMetadataSelection($event: CatalogRecord) {
+  }
 }
